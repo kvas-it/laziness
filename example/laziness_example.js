012f6cf246ef4cb3510f5dd4f84e977966ad2a3a
Vasily Kuznetsov <kvas.it@gmail.com>
2014-11-03 11:38:12 +0100

    Added a simple example.

--- example/laziness_example.js
+++ example/laziness_example.js
@@ -1,5 +1,27 @@
 'use strict';
 
 var L = require('../lib/laziness.js');
+var child_process = require('child_process');
 
-// Nothing for now.
+var hello = L.func(function (name) {
+    console.log('Hello, ' + name + '!');
+});
+
+var byebye = L.func(function (name) {
+    console.log('Bye bye, ' + name + '!');
+});
+
+var process = L.func(function (input) {
+    return input[0].substring(0, input[0].length - 1);
+});
+
+var whoami = L.nfunc(child_process.exec)('whoami');
+var name = process(whoami);
+var greeting = L.If(1,
+        hello(name),
+        byebye(name));
+
+console.log('Waking up.');
+greeting.then(function () {
+    console.log('Done.');
+});
